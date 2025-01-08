94ffdc337635e6404c79849efc4c040a07786965
mainogiulia <giuliamaino97@gmail.com>
2025-01-08 17:00:37 +0100

    esd1w6

--- src/main/java/it/epicode/es/d1w6/author/AuthorController.java
+++ src/main/java/it/epicode/es/d1w6/author/AuthorController.java
@@ -1,14 +1,47 @@
 package it.epicode.es.d1w6.author;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/author")
+@RequestMapping("/api/authors")
 public class AuthorController {
+    @Autowired
+    private AuthorService authorService;
+
+    //metodo per visualizzare tutti gli autori
+    @GetMapping
+    public ResponseEntity<List<Author>> listAllAuthors() {
+        return ResponseEntity.ok(authorService.findAll());
+    }
+
+    //metodo per trovare un autore
+    @GetMapping("/{id}")
+    public ResponseEntity<Author> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(authorService.findById(id));
+    }
+
+    //metodo per creare un autore
+    @PostMapping
+    public ResponseEntity<Author> createAuthor(@RequestBody Author author) {
+        return ResponseEntity.ok(authorService.createAuthor(author));
+    }
+
+    //metodo per aggiornare un autore
+    @PutMapping("/{id}")
+    public ResponseEntity<Author> updateAuthor(@PathVariable Long id, @RequestBody Author authorModificato) {
+        return ResponseEntity.ok(authorService.updateAuthor(id, authorModificato));
+    }
+
+    //metodo per eliminare un autore
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAuthor(@PathVariable Long id) {
+        authorService.deleteAuthor(id);
+        return ResponseEntity.noContent().build();
+    }
+
 
 }
--- src/main/java/it/epicode/es/d1w6/author/AuthorService.java
+++ src/main/java/it/epicode/es/d1w6/author/AuthorService.java
@@ -1,4 +1,46 @@
 package it.epicode.es.d1w6.author;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class AuthorService {
+
+    private final AuthorRepo authorRepo;
+
+    //metodo per visualizzare tutti i BlogPost
+    public List<Author> findAll() {
+        return authorRepo.findAll();
+    }
+
+    //metodo per trovare un BlogPost
+    public Author findById(Long id) {
+        if(!authorRepo.existsById(id)) {
+            throw new EntityNotFoundException("l'autore non è stato trovato");
+        }
+        return authorRepo.findById(id).get();
+    }
+
+    //metodo per creare un BlogPost
+    public Author createAuthor(Author author) {
+        return authorRepo.save(author);
+    }
+
+    //metodo per aggiornare un BlogPost
+
+    public Author updateAuthor(Long id, Author authorModificato) {
+        Author autrModificato = findById(id);
+        BeanUtils.copyProperties(authorModificato,authorModificato);
+        return authorRepo.save(authorModificato);
+    }
+
+    //metodo per eliminare un BlogPost
+    public void deleteAuthor(Long id) {
+        authorRepo.deleteById(id);
+    }
+
 }
